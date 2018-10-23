08f7ad0c16e14f03af8c09ca9de34c5832dac078
jurenqi <jurenqi123520>
2018-10-23 11:52:34 +0800

    整体布局

---
+++ src/main/java/com/rq/badbeans/BadbeansApplication.java
@@ -0,0 +1,12 @@
+package com.rq.badbeans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadbeansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadbeansApplication.class, args);
+	}
+}
