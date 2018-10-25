2096c7376eefb1f7c375d19a0483fa324070f00d
jurenqi <jurenqi123520>
2018-10-25 13:55:23 +0800

    整体布局

--- src/main/java/com/rq/badbeans/BadbeansApplication.java
+++ src/main/java/com/rq/badbeans/BadbeansApplication.java
@@ -2,11 +2,22 @@ package com.rq.badbeans;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BadbeansApplication {
+@ServletComponentScan
+public class BadbeansApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BadbeansApplication.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// 注意这里要指向原先用main方法执行的Application启动类
+		return builder.sources(BadbeansApplication.class);
+	}
+
 }
